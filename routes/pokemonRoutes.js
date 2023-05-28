622e7606662a32ddb63db58f7f4c342a1304bb5c
Kylian Thezenas <kylian.thezenas@etu.umontpellier.fr>
2023-05-28 14:13:21 +0200

    modif auth

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -6,7 +6,7 @@ const pokemonController = require('../controllers/pokemonController');
 router.get('/api/pokemon', pokemonController.allPokemons);
 router.post('/api/pokemon/', auth, pokemonController.addPokemon);
 router.get('/api/pokemon/:number', pokemonController.getPokemon);
-router.put('/api/pokemon/:number', pokemonController.updatePokemon);
+router.put('/api/pokemon/:number', auth, pokemonController.updatePokemon);
 router.delete('/api/pokemon/:number', pokemonController.deletePokemon);
 
 module.exports = router;
